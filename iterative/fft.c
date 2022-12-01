484a256132e1f43b60cb629de4c435fa1f826732
Farooq <farooq@teqniqly.com>
2022-12-01 00:48:04 -0800

    Better iterative FFT.

--- iterative/fft.c
+++ iterative/fft.c
@@ -3,101 +3,82 @@
 #include <math.h>
 #include "fft.h"
 
-void recursiveFFT(struct complex *inputArray, int length) {
-    int n = length;
-    int half = n / 2;
 
-    if (n == 1) {
-        return;
-    }
-
-    //allocate memory of even and odd
-    struct complex *oddArray = malloc(half * sizeof(struct complex));
-    struct complex *evenArray = malloc(half * sizeof(struct complex));
-    for (int i = 0; 2 * i < n; i++) {
-        //Copy even and odd to separate Arrays
-        evenArray[i].real = inputArray[i * 2].real;
-        evenArray[i].imag = inputArray[i * 2].imag;
-        oddArray[i].real = inputArray[i * 2 + 1].real;
-        oddArray[i].imag = inputArray[i * 2 + 1].imag;
-    }
-    //recursive call halves until size of arrays are n == 1
-
-    recursiveFFT(evenArray, half);
-    recursiveFFT(oddArray, half);
-    //printf("array size: %i\n",n);
-    //double angle = 2 * M_PI / n;
-    for (int i = 0; i < n / 2; i++) {
-        struct complex w;
-        w.real = cos(2.0 * M_PI * (double) (i) / (double) (n));
-        w.imag = -sin(2.0 * M_PI * (double) (i) / (double) (n));
-        struct complex mulipliedResult = MultiplyComplex(w, oddArray[i]);
-        inputArray[i].real = evenArray[i].real + mulipliedResult.real;
-        inputArray[i].imag = evenArray[i].imag + mulipliedResult.imag;
-        inputArray[i + n / 2].real = evenArray[i].real - mulipliedResult.real;
-        inputArray[i + n / 2].imag = evenArray[i].imag - mulipliedResult.imag;
-    }
-
-    free(evenArray);
-    free(oddArray);
-    //printComplexArray(inputArray, n);
+// Adapted from https://github.com/rshuston/FFT-C
+void iterativeFFT(struct complex *inputArray, int length)
+{
+    shuffle(inputArray, length);
+    evaluate(inputArray, length);
 }
 
-// Adapted from http://shorturl.at/msuy5
-void iterativeFFT(struct complex *inputArray, struct complex *outputArray, int length) {
-    if (length == 1) {
-        outputArray[0] = inputArray[0];
-        return;
-    }
+// Adapted from https://github.com/rshuston/FFT-C
+void shuffle(struct complex *data, int length)
+{
+    int N = 1 << length;
+    int Nd2 = N >> 1;
+    int Nm1 = N - 1;
+    int i;
+    int j;
 
-    int halfLength = length / 2;
-
-    for (int skip = length, i = 0, shuffle = 0; i < halfLength; i++) {
-        struct complex evenArray = inputArray[shuffle];
-        struct complex oddArray = inputArray[shuffle + halfLength];
-
-        outputArray[i * 2] = AddComplex(evenArray, oddArray);
-        outputArray[i * 2 + 1] = SubtractComplex(evenArray, oddArray);
-
-        if (i == 0 || isPowerOf2(i + 1)) {
-            skip /= 2;
-            shuffle = skip / 2;
-        } else {
-            shuffle += skip;
+    for (i = 0, j = 0; i < N; i++) {
+        if (j > i) {
+            struct complex tmp = data[i];
+            data[i] = data[j];
+            data[j] = tmp;
         }
+
+        int lszb = ~i & (i + 1);
+        int mszb = Nd2 / lszb;
+        int bits = Nm1 & ~(mszb - 1);
+        j ^= bits;
     }
+}
 
-    for (unsigned int evenToOdd = 2; evenToOdd < length; evenToOdd *= 2) {
-        double angle = -M_PI / evenToOdd;
+// Adapted from https://github.com/rshuston/FFT-C
+void evaluate(struct complex *data, int length)
+{
+    unsigned N;
+    unsigned r;
+    unsigned m, md2;
+    unsigned n, k;
+    unsigned i_e, i_o;
+    double theta_2pi;
+    double theta;
+    struct complex Wm, Wmk;
+    struct complex u, t;
 
-        struct complex partialRotation;
-        partialRotation.real = cos(angle);
-        partialRotation.imag = sin(angle);
+    N = 1 << length;
+    theta_2pi = -M_PI;
+    theta_2pi *= 2;
 
-        struct complex currentRotation;
-        currentRotation.real = 1;
-        currentRotation.imag = 0;
+    for (r = 1; r <= length; r++)
+    {
+        m = 1 << r;
+        md2 = m >> 1;
+        theta = theta_2pi / m;
+        Wm.real = cos(theta);
+        Wm.imag = sin(theta);
 
-        for (unsigned int i = 0, toEven = 0; i < halfLength; i++, toEven++) {
-            if (i % evenToOdd == 0) {
-                toEven = 2 * i;
-                currentRotation.real = 1;
-                currentRotation.imag = 0;
+        for (n = 0; n < N; n += m)
+        {
+            Wmk.real = 1.f;
+            Wmk.imag = 0.f;
+            for (k = 0; k < md2; k++)
+            {
+                i_e = n + k;
+                i_o = i_e + md2;
+                u.real = data[i_e].real;
+                u.imag = data[i_e].imag;
+                t = MultiplyComplex(Wmk, data[i_o]);
+                data[i_e].real = u.real + t.real;
+                data[i_e].imag = u.imag + t.imag;
+                data[i_o].real = u.real - t.real;
+                data[i_o].imag = u.imag - t.imag;
+                t = MultiplyComplex(Wmk, Wm);
+                Wmk = t;
             }
-
-            struct complex *even = outputArray + toEven;
-            struct complex *odd = even + evenToOdd;
-
-            const struct complex oddRotated = MultiplyComplex(*odd, currentRotation);
-            *odd = SubtractComplex(*even, oddRotated);
-            *even = AddComplex(*even, oddRotated);
-            currentRotation = MultiplyComplex(currentRotation, partialRotation);
         }
     }
-}
-
-_Bool isPowerOf2(int n) {
-    return (n & n - 1) == 0;
 }
 
 void PrintComplexArray(struct complex *printingArray, int length) {
@@ -112,20 +93,6 @@ struct complex MultiplyComplex(struct complex a, struct complex b) {
     struct complex result;
     result.real = (a.real * b.real - a.imag * b.imag);
     result.imag = (a.real * b.imag + a.imag * b.real);
-    return result;
-}
-
-struct complex AddComplex(struct complex a, struct complex b) {
-    struct complex result;
-    result.real = a.real + b.real;
-    result.imag = a.imag + b.imag;
-    return result;
-}
-
-struct complex SubtractComplex(struct complex a, struct complex b) {
-    struct complex result;
-    result.real = a.real - b.real;
-    result.imag = a.imag - b.imag;
     return result;
 }
 
