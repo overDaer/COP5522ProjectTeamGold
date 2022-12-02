4e9cd58b68fa47e6d754bc796a75751471d2d848
Farooq <farooq@teqniqly.com>
2022-12-02 15:49:15 -0800

    Added folder for files to be submitted.

---
+++ submission/fft.c
@@ -0,0 +1,139 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "fft.h"
+
+void recursiveFFT(struct complex *inputArray, int length) {
+    int n = length;
+    int half = n / 2;
+
+    if (n == 1) {
+        return;
+    }
+
+    //allocate memory of even and odd
+    struct complex *oddArray = malloc(half * sizeof(struct complex));
+    struct complex *evenArray = malloc(half * sizeof(struct complex));
+    for (int i = 0; 2 * i < n; i++) {
+        //Copy even and odd to separate Arrays
+        evenArray[i].real = inputArray[i * 2].real;
+        evenArray[i].imag = inputArray[i * 2].imag;
+        oddArray[i].real = inputArray[i * 2 + 1].real;
+        oddArray[i].imag = inputArray[i * 2 + 1].imag;
+    }
+    //recursive call halves until size of arrays are n == 1
+
+    recursiveFFT(evenArray, half);
+    recursiveFFT(oddArray, half);
+    //printf("array size: %i\n",n);
+    //double angle = 2 * M_PI / n;
+    for (int i = 0; i < n / 2; i++) {
+        struct complex w;
+        w.real = cos(2.0 * M_PI * (double) (i) / (double) (n));
+        w.imag = -sin(2.0 * M_PI * (double) (i) / (double) (n));
+        struct complex mulipliedResult = MultiplyComplex(w, oddArray[i]);
+        inputArray[i].real = evenArray[i].real + mulipliedResult.real;
+        inputArray[i].imag = evenArray[i].imag + mulipliedResult.imag;
+        inputArray[i + n / 2].real = evenArray[i].real - mulipliedResult.real;
+        inputArray[i + n / 2].imag = evenArray[i].imag - mulipliedResult.imag;
+    }
+
+    free(evenArray);
+    free(oddArray);
+}
+
+// Adapted from https://github.com/rshuston/FFT-C
+void iterativeFFT(struct complex *inputArray, int length) {
+    shuffle(inputArray, length);
+    evaluate(inputArray, length);
+}
+
+// Adapted from https://github.com/rshuston/FFT-C
+void shuffle(struct complex *data, int length) {
+    int N = 1 << length;
+    int Nd2 = N >> 1;
+    int Nm1 = N - 1;
+    int i;
+    int j;
+
+    for (i = 0, j = 0; i < N; i++) {
+        if (j > i) {
+            struct complex tmp = data[i];
+            data[i] = data[j];
+            data[j] = tmp;
+        }
+
+        int lszb = ~i & (i + 1);
+        int mszb = Nd2 / lszb;
+        int bits = Nm1 & ~(mszb - 1);
+        j ^= bits;
+    }
+}
+
+// Adapted from https://github.com/rshuston/FFT-C
+void evaluate(struct complex *data, int length) {
+    unsigned N;
+    unsigned r;
+    unsigned m, md2;
+    unsigned n, k;
+    unsigned i_e, i_o;
+    double theta_2pi;
+    double theta;
+    struct complex Wm, Wmk;
+    struct complex u, t;
+
+    N = 1 << length;
+    theta_2pi = -M_PI;
+    theta_2pi *= 2;
+
+    for (r = 1; r <= length; r++) {
+        m = 1 << r;
+        md2 = m >> 1;
+        theta = theta_2pi / m;
+        Wm.real = cos(theta);
+        Wm.imag = sin(theta);
+
+        for (n = 0; n < N; n += m) {
+            Wmk.real = 1.f;
+            Wmk.imag = 0.f;
+            for (k = 0; k < md2; k++) {
+                i_e = n + k;
+                i_o = i_e + md2;
+                u.real = data[i_e].real;
+                u.imag = data[i_e].imag;
+                t = MultiplyComplex(Wmk, data[i_o]);
+                data[i_e].real = u.real + t.real;
+                data[i_e].imag = u.imag + t.imag;
+                data[i_o].real = u.real - t.real;
+                data[i_o].imag = u.imag - t.imag;
+                t = MultiplyComplex(Wmk, Wm);
+                Wmk = t;
+            }
+        }
+    }
+}
+
+void PrintComplexArray(struct complex *printingArray, int length) {
+    int n = length;
+    for (int i = 0; i < n; i++) {
+        printf("[%i]: %lf, %lfi\n", i, printingArray[i].real, printingArray[i].imag);
+    }
+}
+
+/*(a + bi) * (c + di) = (ac − bd) + (ad + bc)i*/
+struct complex MultiplyComplex(struct complex a, struct complex b) {
+    struct complex result;
+    result.real = (a.real * b.real - a.imag * b.imag);
+    result.imag = (a.real * b.imag + a.imag * b.real);
+    return result;
+}
+
+/*A sine wave of frequency f with amplitude r and phase θ is equivalently a complex number r*cos(θ) + ir*sin(θ) */
+struct complex *GenerateSineArray(int samples, int frequency, int amplitude) {
+    struct complex *sineArray = malloc(samples * sizeof(struct complex));
+    for (int i = 0; i < samples; i++) {
+        sineArray[i].real = amplitude * cos(frequency * 2 * M_PI * i / samples);
+        sineArray[i].imag = amplitude * sin(frequency * 2 * M_PI * i / samples);
+    }
+    return sineArray;
+}
---
+++ submission/fft_serial.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+#include <string.h>
+#include "fft.h"
+#include "microtime.h"
+
+int main(int argc, char const *argv[]) {
+    if (argc < 2 || argc > 3) {
+        fprintf(stderr, "USAGE: %s [power of 2] [--print]\n", argv[0]);
+        exit(-1);
+    }
+
+    int powerOf2 = atoi(argv[1]);
+    int length = pow(2, powerOf2);
+
+    printf("Input is of length %d\n", length);
+
+    bool printOutput = false;
+
+    if (argc == 3) {
+        if (strcmp(argv[2], "--print") == 0) {
+            printOutput = true;
+        }
+    }
+
+    struct complex *sumWave = malloc(length * sizeof(struct complex));
+    //create arbitrary sine waves to sum
+    struct complex *sineWave1 = GenerateSineArray(length, 1, 10);
+    struct complex *sineWave2 = GenerateSineArray(length, 2, 4);
+    struct complex *sineWave3 = GenerateSineArray(length, 4, 5);
+    //add sine waves into complex sumWave
+    for (int i = 0; i < length; i++) {
+        sumWave[i].real = sineWave1[i].real + sineWave2[i].real + sineWave3[i].real;
+        sumWave[i].imag = sineWave1[i].imag + sineWave2[i].imag + sineWave3[i].imag;
+    }
+
+    if (printOutput) {
+        printf("Printing input Array\n");
+        PrintComplexArray(sumWave, length);
+    }
+
+    double startTime = microtime();
+    recursiveFFT(sumWave, length);
+    double endTime = microtime();
+
+    if (printOutput) {
+        printf("Printing output Array\n");
+        PrintComplexArray(sumWave, length);
+    }
+
+    free(sumWave);
+    free(sineWave1);
+    free(sineWave2);
+    free(sineWave3);
+
+    printf("\nTime = %g ms\n", (endTime - startTime) / 1000);
+    printf("Timer Resolution = %g us\n", getMicrotimeResolution());
+    /* code */
+    return 0;
+}
+
